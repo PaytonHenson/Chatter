e067f9e7f44b7ce781f053f8f00d4a8187dbb6fa
Payton Henson <gator293@gmail.com>
2017-10-30 22:50:19 -0400

    Server now takes proper command line args

--- src/chatter/Server.java
+++ src/chatter/Server.java
@@ -114,13 +114,11 @@ public class Server {
 	
     /*
      *  To run as a console application just open a console window and: 
-     * > java Server
-     * > java Server portNumber
-     * If the port number is not specified 1500 is used
+     *  > java Server portNumber
      */ 
     public static void main(String[] args) {
         // start server on port 1500 unless a PortNumber is specified 
-        int portNumber = 1500;
+        int portNumber;
         switch(args.length) {
             case 1:
                 try {
@@ -131,7 +129,6 @@ public class Server {
                     System.out.println("Usage is: > java Server [portNumber]");
                     return;
                 }
-            case 0:
                 break;
             default:
                 System.out.println("Usage is: > java Server [portNumber]");
